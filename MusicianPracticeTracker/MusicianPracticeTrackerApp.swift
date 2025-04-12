d908e34f428504aa79a00903033abade2383bfcf
IgorOdaryuk <odaryukigor@gmail.com>
2025-04-12 12:06:52 +0300

    Initial Commit

---
+++ MusicianPracticeTracker/MusicianPracticeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicianPracticeTrackerApp.swift
+//  MusicianPracticeTracker
+//
+//  Created by Igor Odaryuk on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicianPracticeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
